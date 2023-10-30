5ab2b4ccb6c297a549ac02e6b413e6106ef6e4b2
Thato <t.chitja369@gmail.com>
2023-10-30 13:36:47 +0200

    Add Licence Index page to view licence plans

---
+++ LicenceService/LicenceService/Controllers/LicenceController.cs
@@ -0,0 +1,30 @@
+﻿using LicenceService.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicenceService.Controllers
+{
+    public class LicenceController : Controller
+    {
+        private readonly LicenceContext _context;
+
+        public LicenceController(LicenceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Retrieve a list of licences from the database
+            var licences = await _context.Licences.ToListAsync();
+
+            if (licences.Count == 0)
+            {
+                ViewBag.Message = "No Licences Registered";
+                return View();
+            }
+
+            return View(licences);
+        }
+    }
+}
