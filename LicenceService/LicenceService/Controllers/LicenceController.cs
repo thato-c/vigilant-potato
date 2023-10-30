3704f80d602b4ed09d788ab77c44469cdfd8a6c3
Thato <t.chitja369@gmail.com>
2023-10-30 14:27:58 +0200

    Add Create Licence Controller, View and ViewModel

--- LicenceService/LicenceService/Controllers/LicenceController.cs
+++ LicenceService/LicenceService/Controllers/LicenceController.cs
@@ -1,4 +1,5 @@
 ﻿using LicenceService.Data;
+using LicenceService.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,6 +26,36 @@ namespace LicenceService.Controllers
             }
 
             return View(licences);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LicenceViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map the ViewModel to Licence entity
+                var Licence = new Models.Licence
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Cost = model.Cost
+                };
+
+                // Add and save the new licence to the database
+                _context.Licences.Add(Licence);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
     }
 }
---
+++ LicenceService/LicenceService/ViewModels/LicenceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LicenceService.ViewModels
+{
+    public class LicenceViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Cost { get; set; }
+    }
+}
