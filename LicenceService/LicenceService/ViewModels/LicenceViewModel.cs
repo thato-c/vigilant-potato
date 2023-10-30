0be7ef060233333de34cb686ddfedba3e981ccab
Thato <t.chitja369@gmail.com>
2023-10-30 15:33:53 +0200

    Update Error Messages

--- LicenceService/LicenceService/ViewModels/LicenceViewModel.cs
+++ LicenceService/LicenceService/ViewModels/LicenceViewModel.cs
@@ -5,17 +5,18 @@ namespace LicenceService.ViewModels
 {
     public class LicenceViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Licence name is required")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Licence Description is required")]
         public string Description { get; set; }
 
-        [Required]
         [Column(TypeName = "decimal(10, 2)")]
+        [Required(ErrorMessage = "Licence Cost is required")]
         public decimal Cost { get; set; }
 
-
+        [Range(1, int.MaxValue)]
+        [Required(ErrorMessage = "Licence Duration is required")]
         public int ValidityMonths { get; set; } = 1;
     }
 }
