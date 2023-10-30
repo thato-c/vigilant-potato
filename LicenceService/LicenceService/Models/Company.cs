9cf9cde163edcd19211d460a9bbfb7f93460e657
Thato <t.chitja369@gmail.com>
2023-10-30 12:36:33 +0200

    Add Company Model

---
+++ LicenceService/LicenceService/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace LicenceService.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+
+        public string CompanyName { get; set;} 
+
+        public string CompanyPhoneNumber { get; set;} = string.Empty;
+
+        public string CompanyEmail { get; set;} = string.Empty;
+    }
+}
